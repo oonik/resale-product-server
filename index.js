d96e08b49da3f733f7d03db19b5adf0f623c83bd
oonik <aniksarkar0177@gmail.com>
2023-07-21 16:17:57 +0600

    data get veryfi user role

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -27,6 +27,8 @@ async function run() {
   try {
     const fanCategoriesCollection = client.db('resaleProduct').collection('fanCategories');
     const allFanCollection = client.db('resaleProduct').collection('allFan');
+    const bookingCollection = client.db('resaleProduct').collection('booking');
+    const usersCollection = client.db('resaleProduct').collection('users');
 
     app.get('/fancategory', async(req, res)=>{
         const query = {};
@@ -40,7 +42,60 @@ async function run() {
         const fans = await allFanCollection.find(query).toArray();
         const result = fans.filter(fan => fan.category_id === id);
         res.send(result)
-        })
+        });
+
+    app.post('/fan', async(req, res)=>{
+        const fan = req.body;
+        const result = await allFanCollection.insertOne(fan);
+        res.send(result)
+    })    
+
+    app.post('/booking', async(req, res)=>{
+       const booking = req.body;
+       const result = await bookingCollection.insertOne(booking);
+       res.send(result);
+    });
+    
+    app.get('/bookings', async(req, res)=>{
+        const email = req.query.email;
+        const query = {email: email};
+        const result = await bookingCollection.find(query).toArray();
+        res.send(result)
+    });
+
+    app.delete('/booking/:id', async(req, res) =>{
+       const id = req.params.id;
+       const filter = {_id: new ObjectId(id)}
+       const result = await bookingCollection.deleteOne(filter);
+       res.send(result)
+    });
+
+    app.get('/users/seller/:email', async(req, res)=>{
+       const email = req.params.email;
+       console.log(email)
+       const query = {email: email};
+       const user = await usersCollection.findOne(query);
+       res.send({isSeller: user?.role === 'seller'})
+    });
+
+    app.get('/users/admin/:email', async(req, res)=>{
+       const email = req.params.email;
+       const query = {email: email};
+       const user = await usersCollection.findOne(query);
+       res.send({isAdmin: user?.role === 'admin'})
+    });
+
+    app.get('/users', async(req, res)=>{
+        const query = {};
+        const result = await usersCollection.find(query).toArray();
+        res.send(result);
+    })
+
+    app.post('/users', async(req, res)=>{
+       const user = req.body;
+       const result = await usersCollection.insertOne(user);
+       res.send(result)
+    })    
 
   } finally {
     
